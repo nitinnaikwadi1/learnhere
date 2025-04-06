b899febd555d0790d1bfb0ede533267f909baeec
nitinnaikwadi1 <nitinnaikwadi1@gmail.com>
2025-04-06 08:28:19 +0530

    build test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,21 +43,17 @@ const RESOURCES = {"assets/AssetManifest.bin": "c95718173cbb24cfac2c68709acc71d6
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "98c6ed2f565d7be54648005d70b5c787",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "4c05ecfab15d5dc889e4aa6825d47334",
+"flutter_bootstrap.js": "8a73b0774ab621a042fec39f0e83b880",
 "icons/Icon-192.png": "2922f2c9802c6d9611a357e10cfb6d26",
 "icons/Icon-512.png": "1f9281ab196f737b5194c7d1292bdf8e",
 "index.html": "3664f4f09f7f12f1faf5f26bcbc539a9",
 "/": "3664f4f09f7f12f1faf5f26bcbc539a9",
 "main.dart.js": "76bd35ca52426eacd93e2d10e7083e54",
-"main.dart.mjs": "b5a3886243a8a50b0de131ab9744943e",
-"main.dart.wasm": "e8c37cc6cd32d164c1aa96457d286a58",
 "manifest.json": "2f01562ea88ffeaaaef18155d2dfac7b",
 "version.json": "bf9b7e925bbbb99253f37c3c67566aca"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
-"main.dart.wasm",
-"main.dart.mjs",
 "index.html",
 "flutter_bootstrap.js",
 "assets/AssetManifest.bin.json",
