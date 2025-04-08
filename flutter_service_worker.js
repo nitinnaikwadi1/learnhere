e661773824acca53e8f9ba228ab870aa8f9c78b3
nitinnaikwadi1 <nitinnaikwadi1@gmail.com>
2025-04-08 11:12:59 +0530

    background audio async call fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,12 +43,12 @@ const RESOURCES = {"assets/AssetManifest.bin": "c95718173cbb24cfac2c68709acc71d6
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "98c6ed2f565d7be54648005d70b5c787",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "8a9336adb3d7dd438dfde8beb9bfb55e",
+"flutter_bootstrap.js": "b708a973e386d111f20170bd3f7c3b35",
 "icons/Icon-192.png": "2922f2c9802c6d9611a357e10cfb6d26",
 "icons/Icon-512.png": "1f9281ab196f737b5194c7d1292bdf8e",
 "index.html": "3664f4f09f7f12f1faf5f26bcbc539a9",
 "/": "3664f4f09f7f12f1faf5f26bcbc539a9",
-"main.dart.js": "32e0defea9ac98ad731afac93ee93d1a",
+"main.dart.js": "f17708360b3e1021a3815ecdb2672760",
 "manifest.json": "2f01562ea88ffeaaaef18155d2dfac7b",
 "version.json": "bf9b7e925bbbb99253f37c3c67566aca"};
 // The application shell files that are downloaded before a service worker can
