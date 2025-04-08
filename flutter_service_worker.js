3a8dd47047532aca2fa2c2c1265fc523d7748952
nitinnaikwadi1 <nitinnaikwadi1@gmail.com>
2025-04-08 08:05:35 +0530

    content audio refactor and improvements

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c95718173cbb24cfac2c68709acc71d6
 "assets/assets/images/dashboard/vegetables.gif": "593fd1cabcab3421f6359b2182cd09c6",
 "assets/assets/images/dashboard/vehicle.gif": "431d5328ae1c96fd7e29c482480625bf",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "f34dfd897d4816f702725940f7c672a0",
+"assets/fonts/MaterialIcons-Regular.otf": "f154be92f685ae0669f33e5d536250ce",
 "assets/NOTICES": "ec04e2d758f4a0004f783afdf4b9026d",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
@@ -43,12 +43,12 @@ const RESOURCES = {"assets/AssetManifest.bin": "c95718173cbb24cfac2c68709acc71d6
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "98c6ed2f565d7be54648005d70b5c787",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "8a73b0774ab621a042fec39f0e83b880",
+"flutter_bootstrap.js": "10aa668e273b8fc7e611c945d08ad774",
 "icons/Icon-192.png": "2922f2c9802c6d9611a357e10cfb6d26",
 "icons/Icon-512.png": "1f9281ab196f737b5194c7d1292bdf8e",
 "index.html": "3664f4f09f7f12f1faf5f26bcbc539a9",
 "/": "3664f4f09f7f12f1faf5f26bcbc539a9",
-"main.dart.js": "76bd35ca52426eacd93e2d10e7083e54",
+"main.dart.js": "32e0defea9ac98ad731afac93ee93d1a",
 "manifest.json": "2f01562ea88ffeaaaef18155d2dfac7b",
 "version.json": "bf9b7e925bbbb99253f37c3c67566aca"};
 // The application shell files that are downloaded before a service worker can
