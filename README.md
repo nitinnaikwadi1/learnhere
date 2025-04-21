6038f60115564a78011e81a9dd2945a6143cd267
nitinnaikwadi1 <nitinnaikwadi1@gmail.com>
2025-04-21 22:40:17 +0530

    background music random on each load and cleanup

---
+++ README.md
@@ -0,0 +1,6 @@
+this is deployed from learnapp - flutter
+# learnhere
+ learn shapes, animals, numbers, and manny more things around.
+
+> check out the app below
+[learnApp](https://nitinnaikwadi1.github.io/learnhere)
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "c95718173cbb24cfac2c68709acc71d6",
-"assets/AssetManifest.bin.json": "02265bb8b7bec6b870f51bbd8a97f10c",
-"assets/AssetManifest.json": "bd54e7a171d559103b40eedaaedc4374",
-"assets/assets/audio/backg_audio.mp3": "c2acaa54bda53adc6f558d982419b052",
+const RESOURCES = {"assets/AssetManifest.bin": "b70efa083a0d5c1298ac5b434689a7d8",
+"assets/AssetManifest.bin.json": "5e29df48cda5f0bfe100d1465523cd89",
+"assets/AssetManifest.json": "ebdc77b861623296bc5061483c4e949e",
 "assets/assets/images/back1.gif": "5f7a147e1cd3191e96cd6e511110bc2a",
 "assets/assets/images/back2.gif": "b0cc70e529101dea0720ae28e17de062",
 "assets/assets/images/dashboard/alphabets.gif": "107850f512e76f1006c2f7f431a7eb49",
@@ -43,12 +42,12 @@ const RESOURCES = {"assets/AssetManifest.bin": "c95718173cbb24cfac2c68709acc71d6
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "98c6ed2f565d7be54648005d70b5c787",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "ff4a100924ffff872253495948c5c4fc",
+"flutter_bootstrap.js": "c38e781f5a16d5394e972c943cf4d532",
 "icons/Icon-192.png": "2922f2c9802c6d9611a357e10cfb6d26",
 "icons/Icon-512.png": "1f9281ab196f737b5194c7d1292bdf8e",
 "index.html": "3664f4f09f7f12f1faf5f26bcbc539a9",
 "/": "3664f4f09f7f12f1faf5f26bcbc539a9",
-"main.dart.js": "a70d61493b4f024eeb1042ebf40c8997",
+"main.dart.js": "cdea1bf2db9e7e7b0a30a8cb4ea0a7ea",
 "manifest.json": "2f01562ea88ffeaaaef18155d2dfac7b",
 "version.json": "bf9b7e925bbbb99253f37c3c67566aca"};
 // The application shell files that are downloaded before a service worker can
