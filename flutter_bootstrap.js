94d25b8e1e4261877a4c13a22e0184dbdb880e56
nitinnaikwadi1 <nitinnaikwadi1@gmail.com>
2025-11-23 23:37:21 +0530

    minor icon updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2508268852"
+    serviceWorkerVersion: "1495992050"
   }
 });
