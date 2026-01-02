d897e3dd2a642bcc4f1735c3bccc129d37948878
nitinnaikwadi1 <nitinnaikwadi1@gmail.com>
2026-01-02 07:42:51 +0530

    ui improvements

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3240163133"
+    serviceWorkerVersion: "2941812565"
   }
 });
